fef4dc5526b2880eb9307a21b533b06291f0384c
marcpave <marc.pascual.vega@gmail.com>
2024-04-26 12:26:50 +0200

    a

--- Javadoc_MarcPascual/doc/type-search-index.js
+++ Javadoc_MarcPascual/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"hotel"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Guest"},{"p":"<Unnamed>","l":"hotel"},{"p":"<Unnamed>","l":"Manager"},{"p":"<Unnamed>","l":"Person"}];updateSearchResults();
